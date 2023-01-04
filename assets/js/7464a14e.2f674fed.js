c38fa69b0edc743de28003b7547cc06cc4e9e72b
anhcraft <huynhduyanh123123@gmail.com>
2023-01-04 13:08:50 +0000

    Deploying to gh-pages from @ phomc/docs@15e0c4fbc214056e14fcf8c00069e870c5e1199c 🚀

---
+++ assets/js/7464a14e.2f674fed.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkphomc_docs=self.webpackChunkphomc_docs||[]).push([[763],{2088:e=>{e.exports=JSON.parse('{"title":"For Players","slug":"/category/for-players","permalink":"/docs/category/for-players","navigation":{"previous":{"title":"Introduction","permalink":"/docs/tensai/intro"},"next":{"title":"Introduction","permalink":"/docs/tensai/player/intro"}}}')}}]);
